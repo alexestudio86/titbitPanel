92c0b268a8ff83a999756daf135a89fa1c301ff2
alexestudio86 <alexestudio86@hotmail.com>
2022-03-19 15:29:05 -0600

    menor fixes applied

--- src/components/ModalConfirmation.vue
+++ src/components/ModalConfirmation.vue
@@ -30,7 +30,7 @@ export default {
     },
     methods: {
         //...mapMutations('patchOrders', ['makeChange']),
-        ...mapActions('patchOrders', ['updateOrder'])
+        ...mapActions('orders', ['updateOrder'])
     }
 
 }
