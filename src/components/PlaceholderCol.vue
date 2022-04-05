f3f82b29b42ddde4013cc06ed8d81f109ac9c528
alexestudio86 <alexestudio86@hotmail.com>
2022-04-05 16:09:54 -0500

    placeholder updated

---
+++ src/components/PlaceholderCol.vue
@@ -0,0 +1,42 @@
+<template>
+  <tr>
+    <th scope="row">
+      <div class="spinner-grow text-secondary" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </th>
+    <td>
+      <div class="spinner-grow text-secondary" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </td>
+    <td>
+      <div class="spinner-grow text-secondary" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </td>
+    <td>
+      <div class="spinner-grow text-secondary" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </td>
+    <td>
+      <div class="spinner-grow text-secondary" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </td>
+    <td>
+      <div class="spinner-grow text-secondary" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+
+  name: 'PlaceholderCol'
+
+}
+</script>
